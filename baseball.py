3fde12bc028cb44011dcf3ddeb3f0d0eb0e77b52
epgarry13 <epgarry1@gmail.com>
2021-04-10 00:25:08 -1000

    verified output
    Added testing, output is as expected

--- baseball.py
+++ baseball.py
@@ -5,13 +5,20 @@ import json
 
 #still need to set minimum number of at bats
 
-#TODO find unlucky players
+#TODO find unlucky players -- get BA/ABs over same period as exit velo and launch_angle data
 #TODO find hot streak players
 #TODO find unlucky teams going against lucky pitchers
 
 today = date.today()
 data = statcast(str(today-timedelta(5)),str(today-timedelta(1)))
-#batterData = statcast_batter(str(today-timedelta(5)), str(today-timedelta(1)))
+
+#CHECK OUTPUT
+# print(data[data['batter'] == 405395.0]['launch_speed'])
+# print(data[data['batter'] == 405395.0]['launch_angle'])
+# print(data[data['batter'] == 405395.0]['babip_value'])
+
+
+
 
 la = data[data['launch_angle'].notna()]
 la = la.groupby(['batter']).mean('launch_angle')['launch_angle'].to_frame(name = 'launch_angle').reset_index() # average launch angle by batter
@@ -19,23 +26,18 @@ la = la.groupby(['batter']).mean('launch_angle')['launch_angle'].to_frame(name =
 ls = data[data['launch_speed'].notna()]
 ls = ls.groupby(['batter']).mean('launch_speed')['launch_speed'].to_frame(name = 'launch_speed').reset_index() # average launch speed by batter
 
-bv = data[data['babip_value'].notna()]
-bv = bv.groupby(['batter']).mean('babip_value')['babip_value'].to_frame(name = 'babip_value').reset_index() # average babip_value by batter
-
 df = la.merge(ls, how='left', on='batter')
-df = df.merge(bv, how='left', on='batter')
 
-df_temp = df.sort_values('launch_speed',ascending = False)
+#sortedDF = df.sort_values('launch_speed',ascending = False)
 
 temptemp = df.head(40) #for testing
 
-# Populate table based on top 20 average launch speeds over the last 4 days
+# Populate json object based on top 20 average launch speeds over the last 4 days
 arr=[]
 new_arr = []
 new_item = {}
 min = 10000
 for index, row in temptemp.iterrows():
-    temp = statcast_batter(str(today-timedelta(5)), str(today-timedelta(1)), player_id=int(row['batter']))
     
     #update min
     if len(arr) < 20 : # only want top 20
@@ -45,7 +47,6 @@ for index, row in temptemp.iterrows():
         tempdict = {}
         tempdict['launch_speed'] = row['launch_speed']
         tempdict['launch_angle'] = row['launch_angle']
-        tempdict['babip_value'] = row['babip_value']
         new_item[playerName] = tempdict
         arr.append(new_item)
 
@@ -63,7 +64,6 @@ for index, row in temptemp.iterrows():
         playerName = batterTable['player_name'].iloc[0]      
         tempdict['launch_speed'] = row['launch_speed']
         tempdict['launch_angle'] = row['launch_angle']
-        tempdict['babip_value'] = row['babip_value']
         new_item[playerName] = tempdict
         arr.append(new_item)
 
@@ -73,20 +73,14 @@ for index, row in temptemp.iterrows():
                 new_arr.append(player)
 
 if new_arr == []:
-    arr = json.dumps(arr)
+    arr = json.dumps(arr, indent=2)
     print(arr)
 else:
-    new_arr = json.dumps(new_arr)
+    new_arr = json.dumps(new_arr, indent=2)
     print(new_arr)
 
 
 
-
-
-
-
-# dict = {'hello': {'hello2': 'hello', 'hello3': 'hello'}}
-# print(list(dict.keys()))
 
 # for col in data.columns:
 #     print(col)
