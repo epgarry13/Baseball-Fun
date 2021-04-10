b158c82d143929ff814f06db4c31f2fa6b229245
epgarry13 <epgarry1@gmail.com>
2021-04-10 09:50:28 -1000

    update fields

--- baseball.py
+++ baseball.py
@@ -26,51 +26,35 @@ la = la.groupby(['batter']).mean('launch_angle')['launch_angle'].to_frame(name =
 ls = data[data['launch_speed'].notna()]
 ls = ls.groupby(['batter']).mean('launch_speed')['launch_speed'].to_frame(name = 'launch_speed').reset_index() # average launch speed by batter
 
+lsa = data[data['launch_speed_angle'].notna()]
+lsa = lsa.groupby(['batter']).mean('launch_speed_angle')['launch_speed_angle'].to_frame(name = 'launch_speed_angle').reset_index()  #launch speed angle
+
+woba = data[data['estimated_woba_using_speedangle'].notna()]
+woba = woba.groupby(['batter']).mean('estimated_woba_using_speedangle')['estimated_woba_using_speedangle'].to_frame(name = 'estimated_woba_using_speedangle').reset_index() # WOBA
+
 df = la.merge(ls, how='left', on='batter')
+df = df.merge(lsa, how='left', on='batter')
+df = df.merge(woba, how='left', on='batter')
 
 #sortedDF = df.sort_values('launch_speed',ascending = False)
 
-temptemp = df.head(40) #for testing
 
 # Populate json object based on top 20 average launch speeds over the last 4 days
 arr=[]
 new_arr = []
 new_item = {}
-min = 10000
-for index, row in temptemp.iterrows():
-    
-    #update min
-    if len(arr) < 20 : # only want top 20
-        new_item = {}
-        batterTable = statcast_batter(str(today-timedelta(5)), str(today-timedelta(1)), player_id=int(row['batter']))
-        playerName = batterTable['player_name'].iloc[0]      
-        tempdict = {}
-        tempdict['launch_speed'] = row['launch_speed']
-        tempdict['launch_angle'] = row['launch_angle']
-        new_item[playerName] = tempdict
-        arr.append(new_item)
+for index, row in df.iterrows():
+    new_item = {}
+    batterTable = statcast_batter(str(today-timedelta(5)), str(today-timedelta(1)), player_id=int(row['batter']))
+    playerName = batterTable['player_name'].iloc[0]      
+    tempdict = {}
+    tempdict['launch_speed'] = row['launch_speed']
+    tempdict['launch_angle'] = row['launch_angle']
+    tempdict['launch_speed_angle'] = row['launch_speed_angle']
+    tempdict['estimated_woba_using_speedangle'] = row['estimated_woba_using_speedangle']
+    new_item[playerName] = tempdict
+    arr.append(new_item)
 
-        #update min if necessary
-        if min > row['launch_speed']:               
-            min = row['launch_speed']
-            nameMin = playerName
-
- 
-    elif row['launch_speed'] > min: # need to remove lowest launch speed to make room
-
-        new_item = {}
-        tempdict = {}
-        batterTable = statcast_batter(str(today-timedelta(5)), str(today-timedelta(1)), player_id=int(row['batter']))
-        playerName = batterTable['player_name'].iloc[0]      
-        tempdict['launch_speed'] = row['launch_speed']
-        tempdict['launch_angle'] = row['launch_angle']
-        new_item[playerName] = tempdict
-        arr.append(new_item)
-
-        new_arr = []
-        for player in arr:
-            if list(player.keys())[0] != nameMin:
-                new_arr.append(player)
 
 if new_arr == []:
     arr = json.dumps(arr, indent=2)
